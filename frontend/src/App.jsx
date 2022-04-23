c80799be1e03aae1d54c88e04ba1124c1dc3de1b
Gabriel Nogueira <gabrielgoncalvesnogueira@gmail.com>
2022-04-23 00:05:18 -0300

    front-end v1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState, useContext } from "react"
+import Register from "./components/Register";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Table from "./components/Table";
+import LeadModal from "./components/LeadModal";
+import { UserContext } from "./context/UserContext";
+
+const App = () => {
+  const [message, setMessage] = useState("");
+  const [token] = useContext(UserContext);
+
+  const getWelcomeMessage = async () => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+      },
+    };
+    const response = await fetch("/api", requestOptions);
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.log("Something went wrong");
+    } else {
+      setMessage(data.message);
+    }
+  };
+
+  useEffect(() => {
+    getWelcomeMessage();
+  }, []);
+  return (
+    <>
+      <Header title={message}/>
+      <div className="columns">
+        <div className="column"></div>
+        <div className="column m-5 is-two-thirds">
+          {!token ? (
+            <div className="columns">
+              <Register/><Login/>
+            </div>
+          ) : (
+            <Table />
+          )}
+        </div>
+        <div className="column"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/ErrorMessage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ErrorMessage = ({ message }) => (
+    <p className="has-text-weigth-bold has-text-danger">{message}</p>
+);
+
+export default ErrorMessage;
